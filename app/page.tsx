b988df88881238aef9a6d50f0e088932c6c6e2bb
Sajjad Abedi <sajadxabedi@gmail.com>
2023-10-15 20:19:22 +0300

    some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="bg-primary p-9 text-6xl font-semibold">Starter</h1>
+    <main className="">
+      <header className="border-gray-200 border-b py-4 shadow-sm">
+        <div className="container mx-auto px-4">
+          <h1 className="font-semibold">Tiny UI</h1>
+        </div>
+      </header>
+      <div className=" min-h-[50vh] w-full flex-col">
+        <h1 className="mb-20 mt-24 text-center text-9xl font-extrabold tracking-tight">
+          The <span className="text-primary">portfolio</span>, your dream
+          portfolio
+        </h1>
+        <button className="bg-white border-gray-200 rounded-xl border p-2 shadow-sm">
+          Button
+        </button>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: { primary: 'color(display-p3 var(--color-primary) / <alpha-value>)' },
+    fontFamily: {
+      sans: ['"inter", sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: "color(display-p3 var(--color-primary) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
